36f02b885bb4a38594d6a184ab0dbfd419c57f8b
chr0527 <1015670600@qq.com>
2019-05-23 14:17:12 +0800

    init

--- src/main/java/com/chryl/netty/init/InitServerHandler.java
+++ src/main/java/com/chryl/netty/init/InitServerHandler.java
@@ -1,8 +1,6 @@
 package com.chryl.netty.init;
 
 
-import com.chryl.netty.config.TcpConfig;
-import com.chryl.netty.handler.HeartbeatServerHandler;
 import com.chryl.netty.handler.server.ServerBusinessHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -12,9 +10,6 @@ import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
-import io.netty.handler.timeout.IdleStateHandler;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * ServerHandler
@@ -36,11 +31,13 @@ public class InitServerHandler extends ChannelInitializer<Channel> {
         pipeline.addLast("decoder", new ObjectDecoder(Integer.MAX_VALUE, ClassResolvers.cacheDisabled(null)));
 
         /**
+         * 服务端server一般没有心跳,client一般有心跳
+         *
          * 超时心跳机制:具有读超时,写超时,读写超时
          */
-        pipeline.addLast(new IdleStateHandler(TcpConfig.READ_IDEL_TIME_OUT, TcpConfig.WRITE_IDEL_TIME_OUT, TcpConfig.ALL_IDEL_TIME_OUT, TimeUnit.SECONDS)); // 1
+//        pipeline.addLast(new IdleStateHandler(TcpConfig.READ_IDEL_TIME_OUT, TcpConfig.WRITE_IDEL_TIME_OUT, TcpConfig.ALL_IDEL_TIME_OUT, TimeUnit.SECONDS)); // 1
         //心跳机制:用来处理超时时，发送心跳
-        pipeline.addLast(new HeartbeatServerHandler()); // 2
+//        pipeline.addLast(new HeartbeatServerHandler()); // 2
         /**
          * 业务处理机制
          */
